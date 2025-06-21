80dbfcd898f0eaa4115d7daa91a30a6c11da300b
Jickson-S <geojickson@gmail.com>
2025-06-21 19:08:34 +0530

    Fix Vite base path for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-center text-blue-500">Hello, GitHub Pages!</h1>
+    <h1 className="text-3xl font-bold text-center text-blue-500">Street Wheels Cars</h1>
   );
 }
 
