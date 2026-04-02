e9bcd4b56ceaa08cd864f4a0b3912ab01866f377
jickson <44333032+Jickson-S@users.noreply.github.com>
2026-04-02 17:41:58 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,11 +3,15 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const userAgent = request.headers.get('user-agent') || '';
 
-  const isMobile = /mobile|android|iphone|ipad|ipod/i.test(userAgent);
+  const isMobile = /android|iphone|ipad|ipod|mobile/i.test(userAgent);
 
   if (isMobile) {
     return NextResponse.redirect('https://streetmobile.vercel.app');
   }
 
-  return NextResponse.redirect('https://streetwheelscars.com');
+  return NextResponse.redirect('streetwheels.vercel.app');
 }
+
+export const config = {
+  matcher: '/',
+};
